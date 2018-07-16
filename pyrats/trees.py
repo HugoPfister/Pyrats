d5c64292430f6f42dc08368a089a907089aa9e40
Hugo Pfister <pfister@loginhz02.iap.fr>
2018-07-16 15:13:00 +0200

    clean up

--- pyrats/trees.py
+++ pyrats/trees.py
@@ -154,10 +154,6 @@ class Forest(object):
                     fathersID = progenitors.fathersID.item()[fathers]
                     fatherID = progenitors.fatherMass.item()[fathers].argmax()
                     fatherID = fathersID[fatherID]
-                    #fathers = pd.concat([self.trees.loc[(self.trees.halo_num == fatherID) & (self.trees.halo_ts == ts-1)] for fatherID in progenitors.fathersID.item()])
-                    #fathers = fathers.loc[fathers.m == fathers.m.max()]
-                    print('working on ts = {:}'.format(ts))
-                    print('current halo is {:} with father {:}'.format(hnum, fatherID))
                     progenitors = pd.concat([progenitors, self.get_main_progenitor(fatherID, ts-1)])
                     return progenitors
 
