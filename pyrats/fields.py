b2eda78dda84fa4dd4e4be7c389a81eac789ba66
Hugo Pfister <pfister@loginhz02.iap.fr>
2020-04-27 09:57:12 +0200

    fields

--- pyrats/fields.py
+++ pyrats/fields.py
@@ -7,7 +7,7 @@
 # - DM with a particle age very negative and a positive ID
 # - Stars with a particle age positive
 # - Using this only remains VERY young stars (negligible) and cloud particles from BH
-
+import yt
 
 def _star(pfilter, data):
     '''
@@ -35,7 +35,7 @@ def _DM(pfilter, data):
 
 
 def young_star(ds):
-    yt.add_particle_filter("young_star", function=fields._young_star,
+    yt.add_particle_filter("young_star", function=_young_star,
                   filtered_type="star")
     ds.add_particle_filter("young_star")  
     return ds
@@ -44,5 +44,5 @@ def _young_star(pfilter, data):
     '''
     Select particles created after the beginning of the simulation,
     that are younger than 10Myr.'''
-    filter = data[('star','particle_age')] < data.ds.arr(10,'Myr')
+    filter = data[('star','star_age')] < data.ds.arr(10,'Myr')
     return filter
